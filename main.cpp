9fc3d923ec85fc68526d028835cee8aa5a9603ee
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-13 11:10:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,3 +41,24 @@ int decimaltobinarymethod1(int n){
     int binary = decimaltobinarymethod1(n);
     cout<<binary<<endl;
 }
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int binarytodecimal(int n){
+     //bitwise method 
+    int decimal = 0;
+    int i = 0;
+    while (n){
+        int bit = (n % 10);
+        decimal = decimal+bit* pow(2,i++);
+        n= n/=10;
+    }
+    return decimal;
+}
+ int main(){
+    int binaryno;
+    cin>>binaryno;
+    cout<<binarytodecimal(binaryno);
+}
